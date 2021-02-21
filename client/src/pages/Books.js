2f3fc7d496f854f2dbb5c0e5ed7eef01857651f4
Jared Lim <jaredlim1@gmail.com>
2021-02-20 17:41:49 -0700

    component and page updates

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -1,60 +1,52 @@
 import React, { useState, useEffect } from "react";
-import DeleteBtn from "../components/DeleteBtn";
 import Jumbotron from "../components/Jumbotron";
-import API from "../utils/API";
-import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
-import { List, ListItem } from "../components/List";
-import { Input, TextArea, FormBtn } from "../components/Form";
-import "./books.css"
+import Form from "../components/Form";
 import SearchResults from "../components/SearchResults";
+
+import API from "../utils/API";
+import "./books.css"
+
 
 function Books() {
   // Setting our component's initial state
   const [books, setBooks] = useState([])
-  const [formObject, setFormObject] = useState({})
+  const [search, setSearch] = useState("")
 
   // Load all books and store them with setBooks
   useEffect(() => {
-    loadBooks()
+    searchBook()
   }, [])
 
+  function makeBook(bookData) {
+    return {
+        _id: bookData.id,
+        title: bookData.volumeInfo.title,
+        authors: bookData.volumeInfo.authors,
+        description: bookData.volumeInfo.description,
+        image: bookData.volumeInfo.imageLinks.thumbnail,
+        link: bookData.volumeInfo.previewLink
+    }
+}
   // Loads all books and sets them to books
-  function loadBooks() {
-    API.getBooks()
-      .then(res => 
-        setBooks(res.data)
-      )
-      .catch(err => console.log(err));
+  function searchBook() {
+    API.getBook(query)
+    .then(res => setBooks(res.data.items.map(bookData => makeBook(bookData))))
+      .catch(err => console.error(err));
   };
-
-  // Deletes a book from the database with a given id, then reloads books from the db
-  function deleteBook(id) {
-    API.deleteBook(id)
-      .then(res => loadBooks())
-      .catch(err => console.log(err));
-  }
 
   // Handles updating component state when the user types into the input field
   function handleInputChange(event) {
     const { name, value } = event.target;
-    setFormObject({...formObject, [name]: value})
+    setSearch({...search, [name]: value})
   };
 
   // When the form is submitted, use the API.saveBook method to save the book data
   // Then reload books from the database
   function handleFormSubmit(event) {
     event.preventDefault();
-    if (formObject.title && formObject.author) {
-      API.saveBook({
-        title: formObject.title,
-        author: formObject.author,
-        synopsis: formObject.synopsis
-      })
-        .then(res => loadBooks())
-        .catch(err => console.log(err));
-    }
-  };
+    searchBook(search);
+};
 
     return (
       <Container fluid>
@@ -68,50 +60,15 @@ function Books() {
         </Row>
         <Row center>
           <Col size="md-5">
-              <form>
-                <Input
-                  onChange={handleInputChange}
-                  name="title"
-                  placeholder="Title (required)"
-                />
-                <FormBtn
-                  // disabled={!(formObject.author && formObject.title)}
-                  onClick={handleFormSubmit}
-                >
-                  Find Books
-                </FormBtn>
-              </form>
+          <Form>
+              search={search}
+              handleInputChange={handleInputChange}
+              handleFormSubmit={handleFormSubmit}
+            </Form>
           </Col>
-          <SearchResults/>
-
-
-
-
-
-
-
-
-          {/* <Col size="md-6 sm-12">
-            <Jumbotron>
-              <h1>Books On My List</h1>
-            </Jumbotron>
-            {books.length ? (
-              <List>
-                {books.map(book => (
-                  <ListItem key={book._id}>
-                    <Link to={"/books/" + book._id}>
-                      <strong>
-                        {book.title} by {book.author}
-                      </strong>
-                    </Link>
-                    <DeleteBtn onClick={() => deleteBook(book._id)} />
-                  </ListItem>
-                ))}
-              </List>
-            ) : (
-              <h3>No Results to Display</h3>
-            )}
-          </Col> */}
+          <SearchResults
+            books={books}
+            />
         </Row>
       </Container>
     );
